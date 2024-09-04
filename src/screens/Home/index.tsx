98ca138d3b57fb9e39abc8c5274bce71650307b7
Murilo Damario <murilo.gcdamario@gmail.com>
2024-09-03 23:22:58 -0300

    refactor: change ScrollView to FlatList

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,4 +1,4 @@
-import { Text, View, TextInput, TouchableOpacity, ScrollView } from 'react-native'
+import { Text, View, TextInput, TouchableOpacity, FlatList } from 'react-native'
 
 import { Participant } from '../../components/Participant'
 
@@ -38,17 +38,23 @@ export function Home() {
         </TouchableOpacity>
       </View>
 
-      <ScrollView showsVerticalScrollIndicator={false}>
-        {
-          participants.map((participant, index) => (
-            <Participant 
-              key={index} 
-              name={participant} 
-              onRemove={() => handleParticipantRemove(participant)}
-            />
-          ))
-        }
-      </ScrollView>
+      <FlatList
+        data={participants}
+        keyExtractor={item => item}
+        renderItem={({item}) =>(
+          <Participant 
+            key={item} 
+            name={item} 
+            onRemove={() => handleParticipantRemove(item)}
+          />
+        )}
+        showsVerticalScrollIndicator={false}
+        ListEmptyComponent={() => (
+          <Text style={styles.listEmptyText}>
+            Ninguém chegou ainda? Adicone um participante a sua lista de presença
+          </Text>
+        )}
+      />      
     </View>
   )
 }
