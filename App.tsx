4d59c561ddf23b3d3ff10daae91d0369ac630003
Murilo Damario <murilo.gcdamario@gmail.com>
2024-09-03 22:25:56 -0300

    feat: implement Home screen and participants component

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
-import { Text } from 'react-native'
+import { Home } from './src/screens/Home'
 
 export default function App() {
   return (
-    <Text>React Native</Text>
+    <Home />
   )
 }
---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,39 @@
+import { Text, View, TextInput, TouchableOpacity } from 'react-native'
+
+import { Participant } from '../../components/Participant'
+
+import { styles } from './styles'
+
+export function Home() {
+  function handleParticipantAdd() {
+    console.log('Adicionar participante')
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.eventName}>
+        Nome do evento
+      </Text>
+      <Text style={styles.eventDate}>
+        Sexta, 4 de Novembro de 2022
+      </Text>
+
+      <View style={styles.form}>
+        <TextInput 
+          style={styles.input} 
+          placeholder='Nome do participante'
+          placeholderTextColor={'#6B6B6B'}
+        />
+
+        <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
+          <Text style={styles.buttonText}>
+            +
+          </Text>
+        </TouchableOpacity>
+      </View>
+
+      <Participant />
+      <Participant />
+    </View>
+  )
+}
